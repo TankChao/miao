1e631cc33420531653d24794913d3666076f1edc
kangchao <tankchao1994@gmail.com>
2020-06-03 15:51:00 +0800

    更新 TankChao-lodash.js

--- lodash/TankChao-lodash.js
+++ lodash/TankChao-lodash.js
@@ -13,28 +13,39 @@ var TankChao = {
 			return false
 		}
 	},
-	chunk: function (ary, size = 1) {
+	chunk: function (array, size = 1) {
 		var a = []
 		var result = []
-		var l = ary.length
+		var l = array.length
 		var n = Math.floor(l / size)
 		for (var i = 0; i < n; i++) {
-			result.push(ary.slice(0, size * n))
+			result.push(array.slice(0, size * n))
 			a.push(result)
 			result.pop()
 		}
 		if (l !== size * n) {
-			result.push(ary.slice(size * n))
+			result.push(array.slice(size * n))
 			a.push(result)
 			result.pop()
 		}
 		return a
 	},
-	compact: function (ary) {
+	compact: function (array) {
 		var result = []
-		for (var i = 0; i < ary, length; i++) {
-			if (ary[i]) {
-				result.push(ary[i])
+		for (var i = 0; i < array, length; i++) {
+			if (array[i]) {
+				result.push(array[i])
+			}
+		}
+		return result
+	},
+	difference: function (array, values) {
+		var result = []
+		for (var v = 0; v < values.length; v++) {
+			for (var a = 0; a < array.length; a++) {
+				if (array[a] == values[v]) {
+					result.push(array[a])
+				}
 			}
 		}
 		return result
