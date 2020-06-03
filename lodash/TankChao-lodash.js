b3dd090779750facf0c0ac7d78ee2090b1b479fe
kangchao <tankchao1994@gmail.com>
2020-06-03 15:17:35 +0800

    更新 TankChao-lodash.js

--- lodash/TankChao-lodash.js
+++ lodash/TankChao-lodash.js
@@ -1,16 +1,32 @@
 var TankChao = {
-	isNull: function(val) {
+	isNull: function (val) {
 		if (val === null) {
 			return true
 		} else {
 			return false
 		}
 	},
-	isNaN: function(val) {
+	isNaN: function (val) {
 		if (val !== val) {
 			return true
 		} else {
 			return false
+		}
+	},
+	chunk: function (ary, size) {
+		var a = []
+		var result = []
+		var l = ary.length
+		var n = Math.floor(l / 2)
+		for (var i = 0; i < n; i++) {
+			result.push(ary.slice(0, 2n))
+			a.push(result)
+			result.pop()
+		}
+		if (l !== 2n) {
+			result.push(ary.slice(2n))
+			a.push(result)
+			result.pop()
 		}
 	},
 }
