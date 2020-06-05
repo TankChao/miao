7d7f5d3db9fa53f9ac8395d98552fc6f2cff62f5
kangchao <tankchao1994@gmail.com>
2020-06-05 09:42:52 +0800

    更新 tankchao-lodash.js

--- lodash/tankchao-lodash.js
+++ lodash/tankchao-lodash.js
@@ -38,13 +38,15 @@ var tankchao = {
 	},
 
 	difference: function (array, ...value) {
-		var result = array
+		var result = []
 		var b = []
-		for (var v = 1; v <= arguments.length; v++) {
+		var bIdx
+		var al = array.length
+		for (var v = 1; v < arguments.length; v++) {
 			b = b.concat(arguments[v])
 		}
-		for (var a = 0; a < array.length; a++) {
-			var bIdx = b.indexOf(array[a])
+		for (var a = 0; a < al; a++) {
+			bIdx = b.indexOf(array[a])
 			if (bIdx == -1) {
 				result.push(array[a])
 			}
